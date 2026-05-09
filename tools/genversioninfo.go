3e7bbb88fa848b86e5895a5375c533d0bd67c4a7
ionv06 <ionvoicu06@gmail.com>
2026-05-09 17:57:22 +0300

    update

--- tools/genversioninfo.go
+++ tools/genversioninfo.go
@@ -28,7 +28,7 @@ func atoi(s string) int {
 	return n
 }
 
-func main() {
+func Genverioninfo() {
 
 	ver := git("describe", "--tags", "--abbrev=0")
 	commit_id := git("rev-parse", "--short", "HEAD")
